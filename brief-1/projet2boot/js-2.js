3a10e7db4188e46aca8baf31d6066dd8d4761a88
Briacva <96231132+Briacva@users.noreply.github.com>
2022-02-06 01:40:45 +0100

    06/02/2022 01h40

---
+++ brief-1/projet2boot/js-2.js
@@ -0,0 +1,56 @@
+$("#modeNuit").click(function () {
+    if ($("body").hasClass("nuit")) {
+        $("body").removeClass("nuit");
+    }
+    else {
+        $("body").addClass("nuit");
+    }
+});
+
+function data() {
+
+    var Prenom = document.getElementById("Prenom").value;
+    console.log(Prenom);
+
+    var Nom = document.getElementById("Nom").value;
+    console.log(Nom);
+
+    var Adressem = document.getElementById("Adresse-mail").value;
+    console.log(Adressem);
+
+    var NumeroTel = document.getElementById("NumeroTel").value;
+    console.log(NumeroTel);
+
+    var Adressep = document.getElementById("Adresse-postale").value;
+    console.log(Adressep);
+
+    var Codep = document.getElementById("Code-postal").value;
+    console.log(Codep);
+
+    var Ville = document.getElementById("Ville").value;
+    console.log(Ville);
+
+    var objetMessage = document.getElementById("objetMessage").value;
+    console.log(objetMessage);
+
+    var message = document.getElementById("message").value;
+    console.log(message);
+}
+
+function digits(chiffres) {
+    var Zevent = chiffres || window.event;
+
+    // Handle paste
+    if (Zevent.type === 'paste') {
+        key = Event.clipboardData.getData('text/plain');
+    } else {
+        // Handle key press
+        var key = Zevent.keyCode || Zevent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /^\d+$/;
+    if (!regex.test(key)) {
+        Zevent.returnValue = false;
+        if (Zevent.preventDefault) Zevent.preventDefault();
+    }
+}
