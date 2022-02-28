81095a1f2e0dbb5fee7fe1b5f0a2d3ef50023b18
Briacva <96231132+Briacva@users.noreply.github.com>
2022-02-28 11:27:41 +0100

    checkIntegers_28.02.2022

---
+++ Exercices JS/checkIntegers_28.02.2022.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50
+
+function checkIntegers(int1, int2) {
+    if (int1 === 50 || int2 === 20 || int1 + int2 === 50) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// checkIntegers(10, 40)
