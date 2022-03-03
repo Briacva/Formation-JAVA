aa64ec3ba1962592aab2e5b504a390da59738ec0
Briacva <96231132+Briacva@users.noreply.github.com>
2022-03-03 21:35:55 +0100

    03.03.2022 21h35

---
+++ Apprendre à programmer en JAVA 3è Edition/creationClasse/src/firstClass.java
@@ -0,0 +1,22 @@
+public class firstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+// Commentaire ligne
+/* Commentaire paragraphe */
+		
+// System.out.print() correspond au "return" en javascript. Le ln indique de passer à la ligne.
+		
+		System.out.println("Hello World !");
+// Pour passer à la ligne on peut aussi utiliser un caractère d'échappement "\n"
+		System.out.println("Passage à la ligne simple");
+		
+		System.out.print("No passage à la ligne");
+		System.out.println("\nPassage à la ligne début");
+		
+		System.out.print("Passage à la ligne fin\n");
+		System.out.print("\nPassage à la ligne début");
+		
+	}
+}
