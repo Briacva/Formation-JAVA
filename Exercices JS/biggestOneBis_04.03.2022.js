7eb8b4afe2953800c81cae2d37c908bda706574a
Briacva <96231132+Briacva@users.noreply.github.com>
2022-03-04 15:02:29 +0100

    biggestOneBis_04.03.2022

---
+++ Exercices JS/biggestOneBis_04.03.2022.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program that accept two integers and display the larger
+
+function biggestOneBis(a, b) {
+    if (a > b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+biggestOneBis(1, 2)
